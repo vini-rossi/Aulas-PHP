9ac3a94f14d5622194810d60727bcdc8ef8b67e2
vini-rossi <vini.mxp@hotmail.com>
2021-10-13 20:33:55 -0300

    main

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="post">
+		<input type="text" name="email" placeholder="E-mail">
+		<input type="password" name="pass" placeholder="Senha">
+		<input type="submit" name="submit" value="Enviar">
+	</form>
+</body>
+</html>
